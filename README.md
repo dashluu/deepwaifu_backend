b1d95096841e224f29f9ca71e132d09048b624eb
Abhijay Deevi <abhideevi@gmail.com>
2025-03-05 10:55:56 -0800

    getting ready to run

---
+++ README.md
@@ -0,0 +1,6 @@
+### Uses FastAPI to run the backend
+
+To run the program:
+
+1. in terminal, pip install "fastapi[standard]"
+2. fastapi dev server.py
--- character_engine.py
+++ character_engine.py
@@ -1,21 +1,24 @@
-from context import Context
-from models import MessageModel
-import asyncio
-from ollama import chat
+from context import Context # convo history
+from models import MessageModel # message from user
+import asyncio # asynch operations
+from ollama import chat # llama3 model
 
 
 class CharacterEngine:
+    # Initialize the model (default = llama3)
     def __init__(self, model="llama3"):
         self._model = model
 
+    # Asynchronous generator
     async def generate_response(self, ctx: Context, message: MessageModel):
-        ctx.add_message({"role": "user", "content": message.text})
+        ctx.add_message({"role": "user", "content": message.text}) # add user's message to convo history
         # Check history
         # print(messages)
-        if len(ctx.messages) % 5 == 0:
+
+        if len(ctx.messages) % 5 == 0: # every 5 messages, injects identity (to reinforce persona)
             ctx.inject_identity()
-        response = chat(model=self._model, messages=ctx.messages, stream=True)
-        ctx.add_message({"role": "assistant", "content": ""})
+        response = chat(model=self._model, messages=ctx.messages, stream=True) # calls model w/ history
+        ctx.add_message({"role": "assistant", "content": ""}) # placeholder for ai response
         for chunk in response:
             ctx.messages[-1]["content"] += chunk["message"]["content"]
             yield chunk["message"]["content"]
